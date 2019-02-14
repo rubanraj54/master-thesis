9c47e6e03da1f96671a4e3f2601db5a498ffe14e
Rubanraj <rubanraj54@gmail.com>
2019-02-14 21:22:53 +0100

    removed temp test scripts

--- mediator/test/Journal.js
+++
@@ -1,3 +0,0 @@
-
-const files = require.context('../models', true, /(Module|Utils)\.js$/)
-console.log(files);
--- mediator/test/acccheck.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const AccelerometerObservation = require('../models/observations/');
-
-mongoose.connect('mongodb://localhost:27017/test', {
-    useNewUrlParser: true
-});
